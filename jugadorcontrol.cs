d5aeaa509fcc3d456b3c40551714832c840baf98
juanakan <36744896+juanakan@users.noreply.github.com>
2019-04-23 16:48:05 +0200

    Add files via upload

--- jugadorcontrol.cs
+++ jugadorcontrol.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class jugadorcontrol : MonoBehaviour {
+public class jugadorcontrol : MonoBehaviour
+{
 
     public float posicionInicialX, posicionInicialY;
 
@@ -14,42 +16,49 @@ public class jugadorcontrol : MonoBehaviour {
     private Animator anim;
     public bool pisa;
     public bool dispara;
-    public float fuerzasalto= 6.5f;
+    public float fuerzasalto = 6.5f;
     private bool saltar;
     //nuevo
     public Transform punterodisparo;
     public GameObject disparoprefab;
     public AudioSource disparoSound;
-    
+    public float magia;
+    public Text magiamax;
+   
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
         anim.SetBool("pisando", pisa);
         anim.SetBool("disparo", dispara);
         PlayerShooting();
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& magia>0)
         {
             dispara = true;
+            magia = magia -1;
+
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
             dispara = false;
         }
-        
-        if (Input.GetKeyDown(KeyCode.UpArrow)&& pisa)
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && pisa)
         {
             saltar = true;
         }
+        magiamax.text = " " + magia.ToString();
 
     }
     void FixedUpdate()
@@ -67,7 +76,8 @@ public class jugadorcontrol : MonoBehaviour {
 
         rb2d.velocity = new Vector2(limitevelocidad, rb2d.velocity.y);
 
-        if (h > 0.1f){
+        if (h > 0.1f)
+        {
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
         if (h < -0.1f)
@@ -77,24 +87,34 @@ public class jugadorcontrol : MonoBehaviour {
         if (saltar)
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
-            rb2d.AddForce(Vector2.up * fuerzasalto,ForceMode2D.Impulse);
+            rb2d.AddForce(Vector2.up * fuerzasalto, ForceMode2D.Impulse);
             saltar = false;
 
         }
-        
-
 
     }
-   
+
     public void PlayerShooting()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && magia > 0)
         {
             Instantiate(disparoprefab, punterodisparo.position, punterodisparo.rotation);
             disparoSound.Play();
 
         }
-        
+
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag == "magia")
+        {
+            magia =10;
+
+
+
+        }
+
     }
 
 
