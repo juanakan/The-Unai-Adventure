20f4cf599e41a63b6bf19a79c44621b400900858
juanakan <36744896+juanakan@users.noreply.github.com>
2019-05-29 23:40:27 +0200

    Add files via upload

---
+++ pulsado.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class pulsado : MonoBehaviour, IPointerDownHandler, IPointerUpHandler{
+    public bool pulso;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pulso = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pulso = false;
+    }
+}
